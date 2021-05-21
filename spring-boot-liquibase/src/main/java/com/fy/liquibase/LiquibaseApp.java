bf14f62b400f7fed6e992404d694fc70304ade0b
administrator <2316696377@qq.com>
2021-05-21 17:03:56 +0800

    liquibase

---
+++ spring-boot-liquibase/src/main/java/com/fy/liquibase/LiquibaseApp.java
@@ -0,0 +1,12 @@
+package com.fy.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApp.class, args);
+    }
+}
